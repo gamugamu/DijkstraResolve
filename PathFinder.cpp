5ec86232cde95c8bac897eeb6a9bdba667bc3d2c
gamugamu <blaackjack@hotmail.com>
2013-05-29 16:29:36 +0200

    initial

---
+++ PathFinder.cpp
@@ -0,0 +1,9 @@
+//
+//  PathFinder.cpp
+//  TestinC++
+//
+//  Created by Abadie, Loïc on 29/05/13.
+//  Copyright (c) 2013 Abadie, Loïc. All rights reserved.
+//
+
+#include "PathFinder.h"
---
+++ PathFinder.h
@@ -0,0 +1,14 @@
+//
+//  PathFinder.h
+//  TestinC++
+//
+//  Created by Abadie, Loïc on 29/05/13.
+//  Copyright (c) 2013 Abadie, Loïc. All rights reserved.
+//
+
+#ifndef __TestinC____PathFinder__
+#define __TestinC____PathFinder__
+
+#include <iostream>
+
+#endif /* defined(__TestinC____PathFinder__) */
